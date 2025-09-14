47f9ab8d76276bfb79e370a0003a2a5688ba03f2
sjacomeb <sjacome@unal.edu.co>
2025-09-13 21:51:02 -0500

    Taller 1 completado

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
+pip3 install --upgrade pip #Actualiza el pip 
+pip3 install -r requirements.txt #Un archivo de text que contiene un listado de los paquetes que voy a instalar (-r read)
 pip3 install -e .
